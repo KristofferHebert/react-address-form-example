6d6ac37c046358c65dc0839d34ea481b5cbbf931
kristofferhebert <kristofferhebert>
2020-11-19 11:32:45 -0800

    Removing unnecessary comments

--- src/App.js
+++ src/App.js
@@ -15,41 +15,4 @@ export const App = () => {
   )
 }
 
-// Components
-
-// Form
-// Input
-/// / InputError
-// Label
-
-// utils
-/// / validation
-
-// Requirements:
-
-//     Create a simple form that has the following REQUIRED fields:
-//         First Name STRING
-//         Last Name STRING
-//         City STRING
-//         Zip NUMBER
-//         Email STRING EMAIL
-//     Each field should have validation to check:
-//         Required **
-//         Zip format **
-//         Email format **
-//     If any fields has validation errors, show a field specific validation error message similar to “Zip code can only be numeric and 5 digits”
-//     There should be one additional validation that checks that zip value of 98107 (zip code in Seattle)
-//          If not a match, Show a validation error of “Address is not correct”
-//     If any field validations errors exist, the submit button should be disabled
-//     Once submitted, alert the results
-//         Format:
-
-// First Name + Last Name
-
-// City, Zip
-
-// Email
-
-// Provide the code to us and we will do a code review and test it.
-
 export default App
